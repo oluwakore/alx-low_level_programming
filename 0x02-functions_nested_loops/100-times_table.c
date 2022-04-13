c8ae8d864febc14862d57b0ec5700ddd0bbe386d
oluwakore <bellooluwatobi16@gmail.com>
2022-04-13 14:55:17 -0700

    task 100 redeploy[

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,29 +19,22 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				
+
 				prod = num * mult;
 
 				if (prod <= 99)
-				{
 					_putchar(' ');
-				}
 				if (prod <= 9)
-				{
 					_putchar(' ');
-				}
 
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else
+				else if (prod <= 99 && prod >= 10)
 				{
-					if (prod <= 99 && prod >= 10)
-					{
-						_putchar((prod / 10) + '0');
-					}
+					_putchar((prod / 10) + '0');
 				}
 				_putchar((prod % 10) + '0');
 			}
