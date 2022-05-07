b5d8531afd68387dfe65d4fdd7a95b06f7b3f1bc
oluwakore <bellooluwatobi16@gmail.com>
2022-05-07 03:01:25 -0700

    new commit for task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,34 +1,30 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Prints the sum of positive numbers
- * @argc: The number of arguments
- * @argv: An array of pointers to the arguments.
+ * main - entry
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
  *
- * Return: If one of the numbers contains symbols that are non-digits - 1.
- *         Else - 0.
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int num, digit, add = 0;
+	int a, li, add = 0, UNLIMITED_COINS[] = {25, 10, 5, 2, 1};
 
-	for (num = 1; num < argc; num++)
-	{
-		for (digit = 0; argv[num][digit]; digit++)
+	if (argc != 2)
+		return (printf("Error\n"), 1);
+	li = atoi(argv[1]);
+	if (li < 0)
+		return (puts("0"), 1);
+	for (a = 0; a < 5; a++)
+		if (li / UNLIMITED_COINS[a])
 		{
-			if (argv[num][digit] < '0' || argv[num][digit] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
+			add += li / UNLIMITED_COINS[a];
+			li %= UNLIMITED_COINS[a];
 		}
-
-		add += atoi(argv[num]);
-	}
-
 	printf("%d\n", add);
-
 	return (0);
 }
