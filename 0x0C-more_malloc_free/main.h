b3861b9f37405af16049b1c08d65491db5638e36
oluwakore <bellooluwatobi16@gmail.com>
2022-05-10 20:30:10 -0700

    header fixed

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -36,6 +38,10 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
+
+int main(int argc, char **argv)
+
+char *big_multiply(char *s1, char *s2)
 
 char *_strpbrk(char *s, char *accept);
 
