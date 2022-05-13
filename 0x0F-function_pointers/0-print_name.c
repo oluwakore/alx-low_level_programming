727916bfe0b14a73c42eaf57afcf6b50e5ba695c
oluwakore <bellooluwatobi16@gmail.com>
2022-05-13 16:07:57 -0700

    task 1 refix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,18 @@
 #include "function_pointers.h"
 
+
 /**
- *print_name - print the name
- *@name: string
- *@f: function to print with
+ * print_name - prints a name
+ * @name: the string name
+ * @f: the printing function pointer
+ * @f: the printing function pointer
  *
- *Return: Void
+ * Return: void
  */
+
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name != '\0' && f != '\0')
-f(name);
+	if (name && f)
+		f(name);
 }
