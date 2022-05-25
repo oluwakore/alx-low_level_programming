254625843422098dd1f4fdcb3497c90ac84dd660
bello oluwatobi <61856590+oluwakore@users.noreply.github.com>
2022-05-25 03:44:35 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,7 +7,7 @@
 
 /**
  * struct list_s - singly linked list
- * @str: memory allocated string
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
@@ -22,7 +22,7 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
-int strlen(char *a);
+int _strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
@@ -30,4 +30,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void _constructor(void) __attribute__ ((constructor));
 
-#endif /*LISTS_H */
+#endif
