09380b154132394ff5cc7caf25a198b1de7f1814
bello oluwatobi <61856590+oluwakore@users.noreply.github.com>
2022-05-25 03:54:41 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,15 +18,17 @@ void _constructor(void) __attribute__ ((constructor));
 
 /**
  * struct list_s - singly linked list
- * @str: string
- * @len: length
- * @next: next node
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t
 
 #endif /*LISTS_H */
