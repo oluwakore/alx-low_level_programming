8fa861f17b2f8700d7c752c231f2a396b7e7f4be
bello oluwatobi <61856590+oluwakore@users.noreply.github.com>
2022-05-25 04:03:58 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,7 +23,7 @@ typedef struct list_s
 
 
 int _putchar(char c);
-int str_len(char *t);
+int _strlen(char *t);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
