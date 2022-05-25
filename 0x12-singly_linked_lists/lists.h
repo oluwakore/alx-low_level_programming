88c79d7c506c242aa203a01bd87da8e9f346684b
bello oluwatobi <61856590+oluwakore@users.noreply.github.com>
2022-05-25 03:50:52 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,14 +5,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+int _putchar(char c);
+int str_len(char *t);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+void _constructor(void) __attribute__ ((constructor));
+
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ * @str: string
+ * @len: length
+ * @next: next node
  */
 typedef struct list_s
 {
@@ -21,13 +29,4 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
-int _strlen(char *s);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-void _constructor(void) __attribute__ ((constructor));
-
-#endif
+#endif /*LISTS_H */
