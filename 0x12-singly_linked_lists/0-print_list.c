60d50c2beae598fbf75ea8694127a69046c27258
bello oluwatobi <61856590+oluwakore@users.noreply.github.com>
2022-05-25 03:40:02 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * strlen - returns the length of a string
+ * @a: the string whose length to check
  *
  * Return: integer length of string
  */
-int _strlen(char *s)
+int _strlen(char *a)
 {
-	int i = 0;
+	int d = 0;
 
-	if (!s)
+	if (!a)
 		return (0);
-	while (*s++)
-		i++;
-	return (i);
+	while (*a++)
+		d++;
+	return (d);
 }
 
 /**
@@ -25,13 +25,13 @@ int _strlen(char *s)
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t d = 0;
 
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		printf("[%d] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
-		i++;
+		d++;
 	}
 	return (i);
 }
